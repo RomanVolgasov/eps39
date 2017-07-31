30a71bf22ad9d4d5260241148ad3d567a2937466
Roman Volgasov <romanvolgasov@gmail.com>
2017-07-31 17:52:06 +0200

    Публикация
    1) Добавил счётчик метрики.
    2) Зарегистрировал в web-master.
    3) Мелкие SEO-правки.
    4) Поправил пути.
    5) Добавил рабочий скрип для меню.

--- html/js/custom.js
+++ html/js/custom.js
@@ -23,16 +23,15 @@
 
 })(document, window);
 
-$(function(){
-  var current = location.pathname;
-  $('#navigation a').each(function(){
-    var $this = $(this);
-    // if the current path is like this link, make it active
-    if($this.attr('href').indexOf(current) !== -1){
-      $this.addClass('active');
+jQuery(function($) {
+  var path = window.location.href; // because the 'href' property of the DOM element is the absolute path
+  $('#navigation a').each(function() {
+    if (this.href === path) {
+      $(this).addClass('active');
     }
-  })
-})
+  });
+});
+
 
 $(document).ready(function() { 
 
